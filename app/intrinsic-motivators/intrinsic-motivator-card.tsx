b69a7daa8cfac53a4a3f89acd9f9bec6620595b1
Andrija Škontra <askontra22@student.unizd.hr>
2025-02-12 21:52:15 +0100

    Update app/intrinsic-motivators/intrinsic-motivator-card.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/intrinsic-motivators/intrinsic-motivator-card.tsx
+++ app/intrinsic-motivators/intrinsic-motivator-card.tsx
@@ -19,8 +19,8 @@ export default function IntrinsicMotivatorCard({ carouselApi }: Props) {
         />
         <h2 className="font-dancing-script text-4xl">Mastery</h2>
         <p className="font-dosis mt-6">
-          Utilizing this intrinsic motivator will require to you to be
-          competative and look be better today than yesterday.
+          Utilizing this intrinsic motivator will require you to be
+          competitive and strive to be better today than yesterday.
         </p>
         <p className="font-dosis text-gray-500 mt-6 text-sm">
           People: Albert Einstein, Michael Jordan, Leonardo da Vinci
